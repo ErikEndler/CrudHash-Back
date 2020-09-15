8ed0229284230e073d9a26aeb34f116102b24972
ErikEndler <45501252+ErikEndler@users.noreply.github.com>
2020-09-15 13:58:17 -0300

    ajuste dto

--- src/main/java/com/CrudHashBack/DTO/SaleDTO.java
+++ src/main/java/com/CrudHashBack/DTO/SaleDTO.java
@@ -1,5 +1,7 @@
 package com.CrudHashBack.DTO;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,11 +11,13 @@ import lombok.Setter;
 @AllArgsConstructor
 public class SaleDTO {
 
-	
+	@JsonIgnore
 	private int id;
 	private String description;
 	private Double value;
+	@JsonIgnore
 	private String hash;
+	@JsonIgnore
 	private boolean integridade;
 
 }
