e722e1864904ba3a13234b6e70073b4006c0980b
Erik Endler <eng.erik.endler@gmail.com>
2020-09-15 17:23:47 -0300

    ajuste json entrada e saida

--- src/main/java/com/CrudHashBack/DTO/SaleDTO.java
+++ src/main/java/com/CrudHashBack/DTO/SaleDTO.java
@@ -1,23 +1,60 @@
 package com.CrudHashBack.DTO;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @AllArgsConstructor
 public class SaleDTO {
 
-	@JsonIgnore
 	private int id;
 	private String description;
 	private Double value;
-	@JsonIgnore
+	@JsonProperty(access = Access.READ_ONLY)
 	private String hash;
-	@JsonIgnore
+	@JsonProperty(access = Access.READ_ONLY)
 	private boolean integridade;
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	public void setHash(String hash) {
+		this.hash = hash;
+	}
+
+	public void setIntegridade(boolean integridade) {
+		this.integridade = integridade;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public String getHash() {
+		return hash;
+	}
+
+	public boolean isIntegridade() {
+		return integridade;
+	}
 
 }
--- src/main/java/com/CrudHashBack/Models/Sale.java
+++ src/main/java/com/CrudHashBack/Models/Sale.java
@@ -5,7 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
-import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +14,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@Table(name="users")
+
 
 public class Sale {
 
